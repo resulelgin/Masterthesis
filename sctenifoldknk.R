3e0874956fc099ef016ea1ab9e0b156daf798c2b
resulelgin <127510607+resulelgin@users.noreply.github.com>
2024-05-15 21:35:08 +0200

    Create sctenifoldknk.R

---
+++ sctenifoldknk.R
@@ -0,0 +1,42 @@
+options(repos = c(CRAN = "https://cran.rstudio.com/"))
+.libPaths("~/R/x86_64-pc-linux-gnu-library/4.4")  # Set local library path
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
+library(remotes)
+# Install packages if not already installed
+if (!requireNamespace("scTenifoldKnk", quietly = TRUE)) {
+  install_github('cailab-tamu/scTenifoldKnk')
+}
+
+if (!requireNamespace("dplyr", quietly = TRUE)) {
+  install.packages('dplyr')
+}
+
+if (!requireNamespace("scTenifoldNet", quietly = TRUE)) {
+  install.packages('scTenifoldNet')
+}
+if (!requireNamespace("igraph", quietly = TRUE)) {
+  install.packages('igraph')
+}
+if (!requireNamespace("rslurm", quietly = TRUE)) {
+  install.packages('rslurm')
+}
+
+library(dplyr)
+library(igraph)
+library(scTenifoldKnk)
+library(scTenifoldNet)
+library(rslurm)
+
+# Read data
+X <- read.csv("/scratch/users/resul.elgin/Saher/sctenifoldknk/youngcolon/youngcolon.csv")
+rownames(X) <- X$X
+X <- X %>%
+select(-X)
+
+X_filtered <- X[rowSums(X) != 0, ]
+
+
+X_hmgcs2ko <- scTenifoldKnk(X_filtered, gKO='Hmgcs2', qc = FALSE)
+saveRDS(X_hmgcs2ko, file="youngcolon_hmgcs2ko.rds")
